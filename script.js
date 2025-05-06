2b75a60e951146e2f2cc63842793880b02ce6c24
Carlo Viloria <carloviloria808@gmail.com>
2025-05-05 16:27:03 -1000

    Finished portfolio, will add any updates later

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for nav links
+
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+
